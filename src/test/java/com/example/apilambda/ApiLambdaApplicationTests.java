4fd905479ae9d17bcfda553a62402c69a2455371
Davi Ferreira <daviferreirart@gmail.com>
2021-12-24 12:38:43 -0300

    init

---
+++ src/test/java/com/example/apilambda/ApiLambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apilambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLambdaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
